3100c968e41f4785084cfbd421f3848b603ff163
Andrey Dyatlov <adyatlov@posteo.net>
2023-09-22 08:34:49 +0200

    Add taiwind and plugins. Add the form

---
+++ src/components/header/header.tsx
@@ -0,0 +1,8 @@
+import { component$ } from "@builder.io/qwik";
+export default component$(() => {
+  return (
+    <header class="py-5">
+      <h1 class="text-3xl font-bold">Knowlem</h1>
+    </header>
+  );
+});
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,112 +1,87 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
-import Counter from "~/components/starter/counter/counter";
-import Hero from "~/components/starter/hero/hero";
-import Infobox from "~/components/starter/infobox/infobox";
-import Starter from "~/components/starter/next-steps/next-steps";
-
 export default component$(() => {
   return (
     <>
-      <Hero />
-      <Starter />
-
-      <div role="presentation" class="ellipsis"></div>
-      <div role="presentation" class="ellipsis ellipsis-purple"></div>
-
-      <div class="container container-center container-spacing-xl">
-        <h3>
-          You can <span class="highlight">count</span>
-          <br /> on me
-        </h3>
-        <Counter />
-      </div>
-
-      <div class="container container-flex">
-        <Infobox>
-          <div q:slot="title" class="icon icon-cli">
-            CLI Commands
+      <div class="w-full rounded">
+        <form class="space-y-4">
+          <div>
+            <label for="what" class="text-sm">
+              What the command does:
+            </label>
+            <input
+              type="text"
+              name="what"
+              placeholder="Show all failed pods in all namespaces"
+              class="w-full rounded"
+            />
           </div>
-          <>
-            <p>
-              <code>npm run dev</code>
-              <br />
-              Starts the development server and watches for changes
-            </p>
-            <p>
-              <code>npm run preview</code>
-              <br />
-              Creates production build and starts a server to preview it
-            </p>
-            <p>
-              <code>npm run build</code>
-              <br />
-              Creates production build
-            </p>
-            <p>
-              <code>npm run qwik add</code>
-              <br />
-              Runs the qwik CLI to add integrations
-            </p>
-          </>
-        </Infobox>
 
-        <div>
-          <Infobox>
-            <div q:slot="title" class="icon icon-apps">
-              Example Apps
-            </div>
-            <p>
-              Have a look at the <a href="/demo/flower">Flower App</a> or the{" "}
-              <a href="/demo/todolist">Todo App</a>.
-            </p>
-          </Infobox>
+          <div>
+            <label for="how" class="text-sm">
+              Command:
+            </label>
+            <input
+              type="text"
+              name="how"
+              placeholder="kubectl get pods --all-namespaces --field-selector=status.phase=Failed"
+              class="w-full rounded font-mono"
+            />
+          </div>
 
-          <Infobox>
-            <div q:slot="title" class="icon icon-community">
-              Community
-            </div>
-            <ul>
-              <li>
-                <span>Questions or just want to say hi? </span>
-                <a href="https://qwik.builder.io/chat" target="_blank">
-                  Chat on discord!
-                </a>
-              </li>
-              <li>
-                <span>Follow </span>
-                <a href="https://twitter.com/QwikDev" target="_blank">
-                  @QwikDev
-                </a>
-                <span> on Twitter</span>
-              </li>
-              <li>
-                <span>Open issues and contribute on </span>
-                <a href="https://github.com/BuilderIO/qwik" target="_blank">
-                  GitHub
-                </a>
-              </li>
-              <li>
-                <span>Watch </span>
-                <a href="https://qwik.builder.io/media/" target="_blank">
-                  Presentations, Podcasts, Videos, etc.
-                </a>
-              </li>
-            </ul>
-          </Infobox>
-        </div>
+          <div>
+            <label for="output" class="text-sm">
+              Output:
+            </label>
+            <textarea
+              name="output"
+              rows={placeholderOutput.split("\n").length}
+              placeholder={placeholderOutput}
+              class="w-full rounded font-mono"
+            ></textarea>
+          </div>
+
+          <div>
+            <label for="details" class="text-sm">
+              Details:
+            </label>
+            <textarea
+              name="details"
+              rows={placeholderDetails.split("\n").length}
+              placeholder={placeholderDetails}
+              class="w-full rounded font-mono"
+            ></textarea>
+          </div>
+
+          <div>
+            <button type="submit" class="btn">
+              Submit
+            </button>
+          </div>
+        </form>
       </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "Welcome to Qwik",
+  title: "Knowlem",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "Knowlem is your CLI commands knowledge engine",
     },
   ],
 };
+
+const placeholderDetails = `1. \`kubectl\`: This is the command-line interface (CLI) tool for interacting with a Kubernetes cluster.
+2. \`get\`: This subcommand is used to fetch and display one or more resources. In this case, the resource type is \`pods\`.
+3. \`pods\`: Specifies the type of resource you want to get. Pods are the smallest deployable units in Kubernetes that can be created and managed.
+4. \`--all-namespaces\`: This flag is used to list resources from all namespaces in the cluster. Without this flag, only resources in the \`default\` namespace or the one specified with the \`-n\` or \`--namespace\` flag would be displayed.
+5. \`--field-selector\`: This flag allows you to select resources based on the values of their fields. In this command, we're filtering pods based on the \`status.phase\` field. 
+6. \`status.phase=Failed\`: This selects pods that have completed their execution but failed for some reason.`;
+const placeholderOutput = `NAMESPACE     NAME                READY   STATUS      RESTARTS   AGE
+namespace-1   job-12345-1-xxxxx   0/1     Error       0          39h
+namespace-1   job-12345-2-yyyyy   0/1     OOMKilled   1          28h
+namespace-2   job-67890-1-zzzzz   0/1     Error       2          20h`;
